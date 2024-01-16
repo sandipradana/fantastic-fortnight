34cecd3eb60a5c3d89ba8e7d5285a03f269666b2
Sandi Pradana <sandipradana.id@gmail.com>
2024-01-16 15:43:54 +0700

    add config gorm skip transaction

--- backend/cmd/vercel/vercel.go
+++ backend/cmd/vercel/vercel.go
@@ -20,7 +20,9 @@ var routerAdaptor http.HandlerFunc
 func init() {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_SSL_MODE"), os.Getenv("DB_TIMEZONE"))
-	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
 
 	adminRepo := repository.NewAdminRepository()
 	adminService := service.NewAdminService(db, adminRepo)
